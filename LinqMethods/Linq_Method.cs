7e79785b5b6aae7e401c247c35a73b7748524f67
Araqelyan Tiran <tirans3777@gmail.com>
2019-03-31 23:21:10 +0400

    Add methods about Enumerable.Aggregate   Method

--- LinqMethods/Linq_Method.cs
+++ LinqMethods/Linq_Method.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace LinqMethods
 {
-    class Linq_Method
+    public static class Linq_Method
     {
+        // Enumerable.Aggregate Method have three overloads
+
+        // 1.In  MyAggrFuncforSum method I will show how is work Aggregate<TSource>(this IEnumerable<TSource>, Func<TSource,TSource,TSource>) overload:
+        //   There we will calculate sum  of elements  sourse :
+        public static int MyAggrFuncforSum(IEnumerable<int> sourse)
+        {
+
+            if (sourse == null)
+                throw new ArgumentNullException();
+            if (sourse.Count() == 0)
+                throw new InvalidOperationException();
+            return sourse.Aggregate((a, b) => a + b);
+            // 1.a=sourse[0] , b = sourse[1] and reszult = sourse[0] + sourse[1];
+            // 2.a=reszult, b=sourse[3] and reszult = a + sourse[3];
+            //   ..................................................
+        }
+
+        // 2.In MyAggrFuncforMultCount method illustrated work of Aggregate<TSource,TAccumulate>(this IEnumerable<TSource>, TAccumulate, Func<TAccumulate,TSource,TAccumulate>) overload:
+        //    This method return count multiple of x in sourse: 
+
+        public static int MyAggrFuncforMultCount(int x, IEnumerable<int> sourse)
+        {
+            if (x == 0)
+                throw new DivideByZeroException();
+            if (sourse == null)
+                throw new ArgumentNullException();
+            return sourse.Aggregate(0, (a, b) => b % x == 0 && b / x != 0 ? ++a : a);
+
+            //1. a=0,b=sourse[0] and reszult will  incrimented a if  b % x == 0 && b / x != 0 is true ,else no chenchged:
+            //2. a=reszult,b=sourse[1] and reszult wil incrimented a if  b % x == 0 && b / x != 0 is true ,else no chenchged:
+            //   ....................................................................................................
+        }
+
+        //1.Ilustrated Aggregate<TSource, TAccumulate, TResult>(this IEnumerable<TSource>, TAccumulate, Func<TAccumulate, TSource, TAccumulate>, Func<TAccumulate, TResult>) overlad:
+        //  This method retun  information about all  elements
+
+        public static string MyAggFuncforState(IEnumerable<int> sourse)
+        {
+            if (sourse == null)
+                throw new ArgumentNullException();
+          return sourse.Aggregate(0, (a, b) => b > 0 ? ++a : a, c => c == sourse.Count() ? "All elements are positive " : c == 0 ? "All elemrnets are negative" : "There are mixed numbers");
+            // 1. a=0,b=sourse[0] and a will incrimented if b>0
+            // 2. a=sourse[0]>0 ? ++a : a ;
+            //        .....................
+            // At last c=a,and reszult equal in this expression  c => c == sourse.Count() ? .....
+        }
     }
 }
--- LinqMethods/Program.cs
+++ LinqMethods/Program.cs
@@ -10,6 +10,11 @@ namespace LinqMethods
     {
         static void Main(string[] args)
         {
+            int[] ints = { 4, 8,5,6,4,9,12,36,9,0,-7,6 };
+            Console.WriteLine(Linq_Method.MyAggFuncforState(ints));
+            Console.WriteLine($"The multipli of 4 in ints is {Linq_Method.MyAggrFuncforMultCount(4,ints)}");
+            Console.WriteLine($"The sum of ints is {Linq_Method.MyAggrFuncforSum(ints)}");
+
         }
     }
 }
