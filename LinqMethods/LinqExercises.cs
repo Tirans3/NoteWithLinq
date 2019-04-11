d36b3ec7e79016413f970b229498f981d0638972
Araqelyan Tiran <tirans3777@gmail.com>
2019-04-11 15:58:11 +0400

    Add PositiveList method

--- LinqMethods/LinqExercises.cs
+++ LinqMethods/LinqExercises.cs
@@ -7,6 +7,26 @@ using System.Threading.Tasks;
 namespace LinqMethods
 {
     class LinqExercises
-    {
+
+    {     
+           //  Exercisese 1
+           //   Write a program in C# Sharp to find the +ve numbers from 
+           //   a list of numbers using two where conditions in LINQ Query.
+
+        public static IEnumerable<int> PositiveList(IEnumerable<int> a)
+        {
+            // 
+            var b =
+            from VrNum in a
+            where VrNum > 0
+            where VrNum < 12
+            select VrNum;
+
+            // By extension methods
+
+            var c = a.Where(i => i > 0 && i < 12);
+            
+            return c;
+        }
     }
 }
